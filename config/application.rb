c57cd4247df7601b6777b5fee6d0f695f79de3d4
Aaron <aaronhourie@gmail.com>
2016-02-25 17:49:49 -0400

    Fixed styling

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Libra
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    #Include Bootstrap fonts for glyphicons
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
